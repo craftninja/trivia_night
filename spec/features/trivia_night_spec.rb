b9f842738d021b5b96b69865bfc20cb500fef31f
EmilyPlatzer <emilyplatzer@gmail.com>
2014-10-18 21:54:34 -0600

    Added bootstrap navbar with logo

--- spec/features/trivia_night_spec.rb
+++ spec/features/trivia_night_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'Trivia Night' do
   scenario 'User is welcomed on home page' do
     visit '/'
-    expect(page).to have_content("Welcome to Trivia Night!")
+    expect(page).to have_content("Trivia Night!")
   end
 end
